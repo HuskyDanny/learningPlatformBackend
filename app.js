9b5eb9cc5a26b1b1c4ae0d993b4c10aedcbe0abb
Junchen Pan <junchenp1018@gmail.com>
2019-07-08 12:41:03 +0800

    cors

--- app.js
+++ app.js
@@ -20,19 +20,8 @@ mongoose
   .then(() => console.log("connected"))
   .catch(error => console.error(error));
 
-const allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
 app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/uploads", express.static("uploads"));
 app.use(
