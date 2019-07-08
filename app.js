74a8bd7e2c2a1dca1dee76a2cffec8685b139319
Junchen Pan <junchenp1018@gmail.com>
2019-07-08 12:26:16 +0800

    cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose
 const allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
 app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
